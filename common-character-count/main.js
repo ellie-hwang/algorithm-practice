397a94cc7f7976012757f7f28a2e954e5d6017cc
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-05 15:55:31 -0700

    Completed common-character-count

--- common-character-count/main.js
+++ common-character-count/main.js
@@ -1,13 +1,30 @@
 function solution(s1, s2) {
-  let count = 0;
-  let array = []
+  let charCountS1 = {};
+  let charCountS2 = {};
+  let commonChar = {};
+  let finalCount = 0;
   for (let i = 0; i < s1.length; i++) {
-    for (let j = 0; j < s2.length; j++) {
-      if (s1[i] === s2[j] && !array.includes(s1[i])) {
-        array.push(s1[i]);
-        count++;
+    if (s1[i] in charCountS1) {
+      charCountS1[s1[i]]++;
+    } else {
+      charCountS1[s1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < s2.length; i++) {
+    if (s2[i] in charCountS2) {
+      charCountS2[s2[i]]++;
+    } else {
+      charCountS2[s2[i]] = 1;
+    }
+  }
+  for (key in charCountS1) {
+    if (key in charCountS2) {
+      if (charCountS1[key] <= charCountS2[key]) {
+        finalCount += charCountS1[key];
+      } else if (charCountS2[key] < charCountS1[key]) {
+        finalCount += charCountS2[key];
       }
     }
   }
-  return count;
+  return finalCount;
 }
