eba6396c3da85b4c1d7d5c36ca6b480864ac3bc6
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-08 23:13:24 -0700

    in progress: sort-by-height pseudocode

---
+++ sort-by-height/main.js
@@ -0,0 +1,15 @@
+/*
+Given an array, rearrange the array from least to greatest without removing or
+repositioning values that are -1
+input: array
+output: array
+- Start at the beginning of the array
+- Is it -1?
+  -yes, then do nothing and move to the next element in the array
+  -no, then d
+
+*/
+
+function solution(a) {
+
+}
