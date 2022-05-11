6931d6534d22dca82da301efeda5d6bd5623e0c1
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-11 14:35:09 -0700

    Completed are-similar

---
+++ are-similar/main.js
@@ -0,0 +1,23 @@
+function solution(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  let aSort = [...a].sort();
+  let bSort = [...b].sort();
+  if (aSort.join('') !== bSort.join('')) {
+    return false;
+  }
+  let diffCount = 0;
+  for (let i = 0; i < a.length; i++) {
+    if ((a[i] !== b[i])) {
+      diffCount += 1;
+    }
+    if (diffCount > 2) {
+      return false;
+    }
+  }
+  if (diffCount <= 2) {
+    return true;
+  }
+  console.log(diffCount);
+}
